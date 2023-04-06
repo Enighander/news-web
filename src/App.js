09ab8d679cd607a71f79019b89b06d27b412e90d
Enighander <ringgo.enighander@gmail.com>
2023-04-07 06:06:14 +0700

    refactoring functional component and using hook method

--- src/App.js
+++ src/App.js
@@ -1,109 +1,92 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 
+function App() {
+  const [articles, setArticles] = useState([]);
+  const [categories, setCategories] = useState([
+    { name: 'Umum', id: 'general' },
+    { name: 'Bisnis', id: 'business' },
+    { name: 'Teknologi', id: 'technology' },
+    { name: 'Hiburan', id: 'entertainment' },
+    { name: 'Olahraga', id: 'sports' },
+  ]);
+  const [selectedCategory, setSelectedCategory] = useState('general');
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const API_KEY = '6b5961f73bde4899be1a768a6a3aeed7';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [],
-      categories: [
-        { name: 'Umum', id: 'general' },
-        { name: 'Bisnis', id: 'business' },
-        { name: 'Teknologi', id: 'technology' },
-        { name: 'Hiburan', id: 'entertainment' },
-        { name: 'Olahraga', id: 'sports' },
-      ],
-      selectedCategory: 'general',
-      searchKeyword: '',
-      API_KEY: '6b5961f73bde4899be1a768a6a3aeed7',
-    };
-  }
+  useEffect(() => {
+    getNewsArticles(selectedCategory);
+  }, [selectedCategory]);
 
-  componentDidMount() {
-    this.getNewsArticles(this.state.selectedCategory);
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.selectedCategory !== prevState.selectedCategory) {
-      this.getNewsArticles(this.state.selectedCategory);
-    }
-  }
-
-  async getNewsArticles(category, keyword) {
-    let url = `https://newsapi.org/v2/top-headlines?country=id&category=${category}&apiKey=${this.state.API_KEY}`;
+  const getNewsArticles = async (category, keyword) => {
+    let url = `https://newsapi.org/v2/top-headlines?country=id&category=${category}&apiKey=${API_KEY}`;
     if (keyword) {
-      url = `https://newsapi.org/v2/top-headlines?country=id&q=${keyword}&apiKey=${this.state.API_KEY}`;
+      url = `https://newsapi.org/v2/top-headlines?country=id&q=${keyword}&apiKey=${API_KEY}`;
     }
     const response = await fetch(url);
     const data = await response.json();
-    this.setState({ articles: data.articles });
-  }
+    setArticles(data.articles);
+  };
 
-  handleCategoryChange = (category) => {
-    this.setState({ selectedCategory: category });
-  }
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category);
+  };
 
-  handleSearchChange = (event) => {
-    this.setState({ searchKeyword: event.target.value });
-  }
+  const handleSearchChange = (event) => {
+    setSearchKeyword(event.target.value);
+  };
 
-  handleSearchSubmit = (event) => {
+  const handleSearchSubmit = (event) => {
     event.preventDefault();
-    this.getNewsArticles(this.state.selectedCategory, this.state.searchKeyword);
-  }
+    getNewsArticles(selectedCategory, searchKeyword);
+  };
 
-  render() {
-    return (
-      <Router>
-        <div className="container">
-          <h1>Berita</h1>
-          <form onSubmit={this.handleSearchSubmit} >
+  return (
+    <Router>
+      <div className="container">
+        <h1>Berita</h1>
+        <form onSubmit={handleSearchSubmit}>
           <Form.Group>
-              <Form.Control type="text" placeholder="Cari Berita" value={this.state.searchKeyword} onChange={this.handleSearchChange} />
+            <Form.Control type="text" placeholder="Cari Berita" value={searchKeyword} onChange={handleSearchChange} />
           </Form.Group>
-          </form>
-          <Nav variant="tabs" defaultActiveKey={this.state.selectedCategory}>
-            {this.state.categories.map((category) => (
-              <Nav.Item key={category.id}>
-                <Nav.Link as={Link} to={`/${category.id}`} eventKey={category.id} onClick={() => this.handleCategoryChange(category.id)}>
-                  {category.name}
-                </Nav.Link>
-              </Nav.Item>
-            ))}
-          </Nav>
-          <Switch>
-            {this.state.categories.map((category) => (
-              <Route key={category.id} path={`/${category.id}`}>
-                <h2>{category.name}</h2>
-                <div className="row">
-                  {this.state.articles.map((article, index) => (
-                    <div className="col-lg-4 col-md-6 mb-4" key={index}>
-                      <Card>
-                        <Card.Img variant="top" src={article.urlToImage} alt={article.title} />
-                        <Card.Body>
-                          <Card.Title>{article.title}</Card.Title>
-                          <Card.Text>{article.description}</Card.Text>
-                          <a href={article.url} target="_blank" rel="noreferrer" className="btn btn-primary">Selengkapnya</a>
-                        </Card.Body>
-                      </Card>
-                    </div>
-                  ))}
-                </div>
-              </Route>
-            ))}
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+        </form>
+        <Nav variant="tabs" defaultActiveKey={selectedCategory}>
+          {categories.map((category) => (
+            <Nav.Item key={category.id}>
+              <Nav.Link as={Link} to={`/${category.id}`} eventKey={category.id} onClick={() => handleCategoryChange(category.id)}>
+                {category.name}
+              </Nav.Link>
+            </Nav.Item>
+          ))}
+        </Nav>
+        <Switch>
+          {categories.map((category) => (
+            <Route key={category.id} path={`/${category.id}`}>
+              <h2>{category.name}</h2>
+              <div className="row">
+                {articles.map((article, index) => (
+                  <div className="col-lg-4 col-md-6 mb-4" key={index}>
+                    <Card>
+                      <Card.Img variant="top" src={article.urlToImage} alt={article.title} />
+                      <Card.Body>
+                        <Card.Title>{article.title}</Card.Title>
+                        <Card.Text>{article.description}</Card.Text>
+                        <a href={article.url} target="_blank" rel="noreferrer" className="btn btn-primary">Selengkapnya</a>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                ))}
+              </div>
+            </Route>
+          ))}
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
-
-
-
